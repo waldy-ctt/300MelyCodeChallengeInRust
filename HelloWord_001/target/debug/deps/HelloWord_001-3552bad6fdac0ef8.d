08484a66dc235c6a14774beef99ee24fd43f6551
waldyctt <clocktoktok@gmail.com>
2024-11-19 16:54:36 +0700

    6

---
+++ HelloWord_001/target/debug/deps/HelloWord_001-3552bad6fdac0ef8.d
@@ -0,0 +1,5 @@
+/home/waldy/Usage/project/300Mely/HelloWord_001/target/debug/deps/libHelloWord_001-3552bad6fdac0ef8.rmeta: src/main.rs
+
+/home/waldy/Usage/project/300Mely/HelloWord_001/target/debug/deps/HelloWord_001-3552bad6fdac0ef8.d: src/main.rs
+
+src/main.rs:
---
+++ HelloWord_001/target/debug/deps/HelloWord_001-fe514217c3b96074.d
@@ -0,0 +1,5 @@
+/home/waldy/Usage/project/300Mely/HelloWord_001/target/debug/deps/libHelloWord_001-fe514217c3b96074.rmeta: src/main.rs
+
+/home/waldy/Usage/project/300Mely/HelloWord_001/target/debug/deps/HelloWord_001-fe514217c3b96074.d: src/main.rs
+
+src/main.rs:
